8ddceee7df2430fe37657b2a5a427760e869bf79
Jake Williamson <jakewilliamson@jakewill01m.corp.skyscanner.local>
2016-07-08 17:09:56 +0100

    Add all the things, do all the work

--- static/index.js
+++ static/index.js
@@ -10,13 +10,16 @@ main = function () {
         method: "GET",
         url: "http://127.0.0.1:5000/list/1",
     })
-    
+
         .done(function (msg) {
             if(msg.list_items.length > 0)
+                var itemsArray =  Array(msg.list_items.length);
                 $(msg.list_items).each(function (i) {
-                    addToList(this.text);
-                })
-
+                    itemsArray[this.location -1] = this.text;
+                });
+            for(var i = itemsArray.length - 1; i >= 0; i){
+            addToList(itemsArray [i]);
+            }
             if(msg.listname.length > 0) {
                 $('.titletext').text(msg.listname);
             }
@@ -26,17 +29,38 @@ main = function () {
         });
 
     function addToList(message) {
-        $('<li>').text(message).prependTo('.list');
-        $('<input type="checkbox" class="list-checkbox"/>').prependTo($('ol li:first'));
-        $('#notes').val("");
-        $('ol').sortable();
+        //$(".to-do-list").append('<div class = "row rowdiv"> </div>');
+        var textDiv = $('<div>')
+                        .addClass('col-md-4')
+                        .addClass('todo-text')
+                        .text(message);
+        var checkBoxDiv = $('<div>')
+                            .addClass('col-md-1')
+                            .addClass('todo-done-checkbox')
+                            .html('<input type="checkbox" class="list-checkbox"/>');
+        var deleteDiv = $('<div>')
+                            .addClass('col-md-1')
+                            .addClass('todo-delete')
+                            .html('<input type="radio" class="todo-delete-radio" value="1" hidden/>')
+        var rowDiv = $('<div>')
+                    .addClass('row')
+                    .addClass('todo-row');
+
+        rowDiv.append(checkBoxDiv).append(textDiv).append(deleteDiv);
+
+        $('.to-do-list').prepend(rowDiv);
+        // $('<input type="checkbox" class="list-checkbox"/>').prependTo($('ol li:first'));.        .text(mes       sage)
+        // $('#notes').val("");
+        $('.to-do-list').sortable();
         setEventListeners();
     }
 
     function setEventListeners() {
         $('.list-checkbox').change(function() {
-            $('.done-list').append($(this).parent())
-            $
+            if($(this).is(':checked'))
+                $('.done-list').append($(this).parents('.todo-row'));
+            else
+                $('.to-do-list').append($(this).parents('.todo-row'));
         });
     }
 
@@ -44,41 +68,37 @@ main = function () {
             var post = $('#notes').val();
             if (post.length > 0) {
                 addToList(post);
+                $('#notes').val("");
+
             }
 
 
         });
 
         $('.btn-del').click(function () {
-
-            $('ol li').each(function(index){
-               if($(this).find('input:radio').length == 0) {
-                   $('<input type="radio" id = rad value="1"/> ').appendTo($(this));
-               }
-            });
-           // if(' ol li').contains('radio')(function() {
-           //      $('<input type="radio" id = rad value="1"/> ').appendTo($('ol li'));
-           //  });
+            $('.todo-delete-radio').show();
             $('.btn-canc').show();
             $('.btn-conf').show();
 
 
 
-        })
+        });
 
         $('.btn-canc').click(function () {
             $('.btn-canc').hide();
             $('.btn-conf').hide();
-            $(":radio").remove();
+            $('.todo-delete-radio').prop('checked', false);
+            $('.todo-delete-radio').hide();
         });
 
         $('.btn-conf').click(function () {
             $("input:radio:checked").each(function() {
-               $(this).parent().remove();
-            })
+               $(this).parents('to-do-row').remove()
+            });
             $('.btn-canc').hide();
             $('.btn-conf').hide();
-            $(":radio").remove();
+            $(":radio").hide();
+            $('.todo-delete-radio').prop('checked', false);
         });
 
 
